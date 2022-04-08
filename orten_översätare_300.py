c955175534040ede95376b763e2aac3c2cec6575
carl <carl.samme@elev.ga.ntig.se>
2022-04-08 13:37:56 +0200

    lol

--- orten_översätare_300.py
+++ orten_översätare_300.py
@@ -7,13 +7,35 @@ Ha med en docstring
 https://www.swedishnomad.com/sv/orten-slang/ 
 https://www.gp.se/nyheter/sverige/35-ord-som-bara-%C3%A4kta-ortenbarn-f%C3%B6rst%C3%A5r-1.2586616
 """
-def convert_to_list(): 
+def convert_word_to_list(): 
     all_orten_words = []
     with open("orten_ordlista.txt", "r", encoding="utf8") as word:
         all_orten_words = word.readlines()
-    print(all_orten_words)
+    #print(all_orten_words)
+    return all_orten_words
+convert_word_to_list()
 
-convert_to_list()
+def convert_sentences_to_list():
+    all_orten_meningar = []
+    with open("orten_meninglista.txt", "r", encoding="utf8") as mening:
+        all_orten_meningar = mening.readlines()
+    #print(all_orten_meningar)
+    return all_orten_meningar 
+
+
+def make_list_better(sentence_list : list(), word_list : list()):
+    new_word_list = []
+    new_sentence_list = []
+    for i in word_list:
+        new_word_list.append(str(i).strip("\n"))
+    for i in sentence_list:
+        new_sentence_list.append(str(i).strip("\n"))
+    # print(new_word_list)
+    # print(new_sentence_list)
+    return new_word_list, new_sentence_list
+
+make_list_better(convert_sentences_to_list(), convert_word_to_list())
+
         
 
 
