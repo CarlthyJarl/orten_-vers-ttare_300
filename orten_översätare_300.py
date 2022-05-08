2af5c4159b2ef6ca50e7b7434706e2b02af97582
carl <carl.samme@elev.ga.ntig.se>
2022-05-08 12:36:21 +0200

    abo den funkar

--- orten_översätare_300.py
+++ orten_översätare_300.py
@@ -9,59 +9,108 @@ class GoodOrBad: #Jag kom på att jag har glömt hur klasseer fungerar
 class Emotion:
     pass
 
+class Gramor: #Vokaler consonanter n shit
+    pass
+
 #Funktioner
 """
-Ha med en docstring
+Ha med en docstring som förklarar complexa saker 
 
 https://www.swedishnomad.com/sv/orten-slang/ 
 https://www.gp.se/nyheter/sverige/35-ord-som-bara-%C3%A4kta-ortenbarn-f%C3%B6rst%C3%A5r-1.2586616
 """
-def convert_word_to_list(): 
+def convert_orten_word_to_list():
     all_orten_words = []
     with open("orten_ordlista.txt", "r", encoding="utf8") as word:
         all_orten_words = word.readlines()
-    #print(all_orten_words)
     return all_orten_words
-#convert_word_to_list()
 
-def convert_sentences_to_list():
+def convert_svenne_word_to_list():
+    all_svenne_words = []
+    with open("svenne_ordlista.txt", "r", encoding="utf8") as word:
+        all_svenne_words = word.readlines()
+    return all_svenne_words
+
+def convert_orten_sentences_to_list():
     all_orten_meningar = []
     with open("orten_meninglista.txt", "r", encoding="utf8") as mening:
         all_orten_meningar = mening.readlines()
-    #print(all_orten_meningar)
     return all_orten_meningar
 
+def convert_svenne_sentences_to_list():
+    all_svenne_meningar = []
+    with open("svenne_meningslista.txt", "r", encoding="utf8") as mening:
+        all_svenne_meningar = mening.readlines()
+    return all_svenne_meningar
 
-def make_list_better(sentence_list : list(), word_list : list()):
-    new_word_list = []
-    new_sentence_list = []
 
-    for i in word_list:
-        new_word_list.append(str(i).strip("\n").lower())
-    for i in sentence_list:
-        new_sentence_list.append(str(i).strip("\n").lower())
+def make_list_better(any_words_or_sentenses_list : list()):
+    new_any_words_or_sentenses_list = []
+    for item in any_words_or_sentenses_list:
+        new_any_words_or_sentenses_list.append(str(item).strip("\n").lower()) #lower fungerar inte om det inte står str typ
+    return new_any_words_or_sentenses_list
+
+
+def translate(sentence : int):
+    orten_word_list = make_list_better(convert_orten_word_to_list())
+    orten_sentence_list = make_list_better(convert_orten_sentences_to_list())
+    svenne_word_list = make_list_better(convert_svenne_word_to_list())
+    svenne_sentence_list = make_list_better(convert_svenne_sentences_to_list())
+
+    sentence_index = -1
+    is_sentence = False
+    sentence = sentence.lower()
+
+    for item in svenne_sentence_list:
+        sentence_index += 1
+        if item == sentence:
+            print(orten_sentence_list[sentence_index])
+            is_sentence = True
+            break
         
-    return new_word_list, new_sentence_list
+    if is_sentence == False:
+        word_index = -1
+        sentence_list = list(sentence.split(" "))
+
+        abo = 0
+
+        for word in sentence_list:
+            word_index += 1
+            orten_index = -1
+            for item in orten_word_list:
+                orten_index += 1
+                if word in item:
+                    sentence_list[word_index] = svenne_word_list[orten_index] #denna är wack
+                    abo += 1
+
+        print(sentence_list)
 
 
-def convert_svenne_words_to_list():
-    pass # är här nu
+
+    #for word in sentence_list:
+        #for equel in svenne 
+        #if word ==
 
 
 
 
-        
 
+ 
 
 #Huvudfunktion 
-def main(orten_list : list()): 
+def main(): 
+
     while True: 
         print("Wellcome to main.")
         print('Press 1 for "Orten Translator" Press 2 for "Svenne Translator" Press 3 to end the program.')
         choise = int(input("- "))
 
         if choise == 1:
-            print(orten_list)
+            print("Whrite your sentence")
+            sentence = input("- ")
+            #print(orten_word_list)
+            #print(orten_senctence_list)
+            translate(sentence)
 
         elif choise == 2:
             print("comming soon")
@@ -71,8 +120,7 @@ def main(orten_list : list()):
 
 
 if __name__ == "__main__":
-    #Här ligger alla importerade ordoch meningr i var sin lista i main
-    main(make_list_better(convert_sentences_to_list(), convert_word_to_list()))
+    main()
 
 #Kom ihåg att converta till lower case
 #Sen ta bort alla svenska ord och ändra till English
@@ -86,3 +134,5 @@ if __name__ == "__main__":
 #Kom ihåg plural form 
 
 #Aanvänd klasser för att hitta känsla eller ton i meningen
+
+#läg till mer orten ord
---
+++ test.py
@@ -0,0 +1,13 @@
+l = ["jänni/jenni", "loral"]
+jänni = "jänni"
+ord = "ord"
+
+l[1] = "abo"
+
+for i in l:
+    if jänni in i:
+        print(ord)
+
+
+
+print(l)
